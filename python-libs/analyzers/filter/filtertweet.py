9be9ae4336399a7f2c5c832cc98a4643b56911b4
eduardo <eduardomesquita>
2014-11-03 13:05:15 -0200

    script

--- python-libs/analyzers/filter/filtertweet.py
+++ python-libs/analyzers/filter/filtertweet.py
@@ -26,3 +26,24 @@ class FilterTweet( object ):
 
         return True
         
+if __name__ == '__main__':
+
+    import  sys, os
+
+    path_python_libs =  '/'.join( sys.path[0].split('/')[:-2] )
+    sys.path.append(path_python_libs + '/connectors/mongo/')
+    
+    from mongojudith import TwitterDB
+    
+    twitter_db =TwitterDB()
+
+    for data in twitter_db.find_raw_data_users():
+        
+        f = FilterTweet()
+        if f.filter(  data ):
+            try:
+                user_name = data['user']['screen_name']
+                response = twitter_db.save_key_words_by_username( user_name  )
+                print response
+            except:
+                print 'ERRO'
